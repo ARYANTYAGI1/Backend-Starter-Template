47d0dffe3235ead66f999a02489e2cca5eb3934e
Aryan Tyagi <aryantyagi296@gmail.com>
2024-12-02 22:14:35 +0530

    added cors

--- server.js
+++ server.js
@@ -3,10 +3,19 @@ const express = require('express');
 const mongoose = require('./config/db');
 const userRoutes = require('./routes/User');
 const todoRoutes = require('./routes/Todo');
+const cors = require('cors');
 const app = express();
 app.use(express.json());
 require('./helpers/cron');
 
+const corsOptions = {
+    origin: 'http://localhost:5173', // Replace with your frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true, // Enable cookies or authorization headers
+  };
+  
+  app.use(cors(corsOptions));
+  
 app.use('/api/users', userRoutes);
 app.use('/api/todo',todoRoutes)
 
